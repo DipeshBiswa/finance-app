5815ca15346c7d77a2a20ed6a05046bb357e474c
Dipesh Biswa <db4048@g.rit.edu>
2026-01-06 23:30:24 -0500

    jwtService implemented

--- src/main/java/com/financeapp/finance_app/service/JwtService.java
+++ src/main/java/com/financeapp/finance_app/service/JwtService.java
@@ -1,15 +1,32 @@
 package com.financeapp.finance_app.service;
 
 import com.financeapp.finance_app.model.user;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
-    private final UserService userService;
-    public JwtService(UserService userService) {
-        this.userService = userService;
+    private String secretKey;
+    private SecretKey key;
+
+    public JwtService(@Value("${jwt.secret}") String secretKey) {
+        this.secretKey = secretKey;
+        byte[] byteArray = secretKey.getBytes();
+        key = Keys.hmacShaKeyFor(byteArray);
     }
 
-    public String generateToken(user user){
-
+    Long expiration = 86400000L;
+    public String generateToken(String username){
+        return Jwts.builder().subject(username).issuedAt(new Date(System.currentTimeMillis())).expiration(new Date(System.currentTimeMillis() + expiration)).signWith(key).compact();
+    }
+    public String extractUsername(String token){
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
     }
 
 
